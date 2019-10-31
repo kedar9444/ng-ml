d579b5b44986ba79dec0c209992745557e7e6a65
Kedar Paritkar <kedar.paritkar@angularway.com>
2019-10-31 14:11:41 +0530

    added support for ml3 animation.

--- projects/ng-moving-letters/src/lib/ng-moving-letters.component.ts
+++ projects/ng-moving-letters/src/lib/ng-moving-letters.component.ts
@@ -36,6 +36,10 @@ export class NgMovingLettersComponent implements AfterViewInit {
         this.ML2();
         break;
 
+      case 'ML3':
+        this.ML3();
+        break;
+
       default:
         this.ML1();
         break;
@@ -78,5 +82,27 @@ export class NgMovingLettersComponent implements AfterViewInit {
 
 
   }
+
+
+  public ML3() {
+    const textWrapper = this.content.nativeElement; // document.querySelector('.ml3');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, '<span class=\'letter\'>$&</span>');
+
+    anime.timeline({ loop: true })
+      .add({
+        targets: '.ml3 .letter',
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 2250,
+        delay: (el, i) => 150 * (i + 1)
+      }).add({
+        targets: '.ml3',
+        opacity: 0,
+        duration: 1000,
+        easing: 'easeOutExpo',
+        delay: 1000
+      });
+  }
+
 
 }
